efb5f257c27ea2ee1093e54fb391b718a91289fa
Nerian <siotopo@gmail.com>
2015-10-14 18:31:31 +0200

    Update assets to 1.5.0-R1

--- vendor/assets/javascripts/bootstrap-datepicker/locales/bootstrap-datepicker.cs.js
+++ vendor/assets/javascripts/bootstrap-datepicker/locales/bootstrap-datepicker.cs.js
@@ -5,14 +5,14 @@
  */
 ;(function($){
 	$.fn.datepicker.dates['cs'] = {
-		days: ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota", "Neděle"],
-		daysShort: ["Ned", "Pon", "Úte", "Stř", "Čtv", "Pát", "Sob", "Ned"],
-		daysMin: ["Ne", "Po", "Út", "St", "Čt", "Pá", "So", "Ne"],
+		days: ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"],
+		daysShort: ["Ned", "Pon", "Úte", "Stř", "Čtv", "Pát", "Sob"],
+		daysMin: ["Ne", "Po", "Út", "St", "Čt", "Pá", "So"],
 		months: ["Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen", "Listopad", "Prosinec"],
 		monthsShort: ["Led", "Úno", "Bře", "Dub", "Kvě", "Čer", "Čnc", "Srp", "Zář", "Říj", "Lis", "Pro"],
 		today: "Dnes",
 		clear: "Vymazat",
 		weekStart: 1,
-		format: "d.m.yyyy"
+		format: "dd.m.yyyy"
 	};
 }(jQuery));
